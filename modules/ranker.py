1d75038c649efeab403cd2e5f3195c747e319d8c
Harshitha Lukka <lukkaharshitha@gmail.com>
2025-07-26 22:41:42 +0530

    Delete modules directory

--- modules/ranker.py
+++
@@ -1,5 +0,0 @@
-from sklearn.metrics.pairwise import cosine_similarity
-
-def rank_resumes(job_embedding, resume_embeddings):
-    scores = cosine_similarity([job_embedding], resume_embeddings)[0]
-    return scores
